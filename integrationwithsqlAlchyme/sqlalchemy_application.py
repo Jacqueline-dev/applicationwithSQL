d9315376d1ad0d6e816380c77d594bacaf6b4da0
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-04-28 17:28:22 -0300

    account class creation

--- integrationwithsqlAlchyme/sqlalchemy_application.py
+++ integrationwithsqlAlchyme/sqlalchemy_application.py
@@ -1,3 +1,5 @@
+from sqlite3 import Binary
+
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, String
@@ -25,7 +27,33 @@ class Customer(Base):
         "Account", back_populates="customer", cascade="all, delete-orphan"
     )
 
-     def __repr__(self):
+    def __repr__(self):
          return f"Customer(id={self.id}, name={self.name}, cpf={self.cpf}, address={self.address})"
 
 
+class Unique:
+    pass
+
+
+class Foreignkey:
+    pass
+
+
+class Account(Base):
+    __tablename__ = "account"
+    id = Collumn(Binary, primary_key=True)
+    account_type = Collumn(String)
+    agency = Collumn(String)
+    number = Collumn(Interge)
+    id_customer = Collumn(Interge, Foreignkey("user_customer.id"), unique=True)
+
+    user = relationship(
+        "customer", back_populates="account"
+    )
+
+    def __repr__(self):
+        return f"Account(id={self.id}, account_type={self.account_type}, agency={self.agency}, number={self.number}, id_customer={self.id_customer})"
+
+
+print(Customer.__tablename__)
+print((Account.__tablename__)
